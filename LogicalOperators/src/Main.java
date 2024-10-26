361abfc179a9dd27f8f55227a64db0d36225390a
MykolaDekusar <ndekusar97@gmail.com>
2024-10-26 18:06:33 +0200

    Test of && (AND) operator

--- LogicalOperators/src/Main.java
+++ LogicalOperators/src/Main.java
@@ -6,6 +6,13 @@ public class Main {
 		// && AND entrambi le condizioni devono essere vere
 		// || OR una delle condizione deve essere vera
 		// ! NOT inverte il valore di una condizione booleana (se era true diventa false e viceversa)
+		//Testiamo l'AND
+		int temperatura= 3;
+		if(temperatura >20 && temperatura <=30) {
+			System.out.println("Si sta bene");
+		}
+		else if(temperatura <=20 && temperatura >=5) System.out.println("Fa fresco");
+		else if (temperatura >=0 && temperatura <5 ) System.out.println("Fa freddo");
 	}
 
 }
