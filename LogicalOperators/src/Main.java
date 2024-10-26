f4a19960c3e76cbad7249222a20129bf98bbd995
MykolaDekusar <ndekusar97@gmail.com>
2024-10-26 17:59:32 +0200

    Initializzation of && (AND) || (OR) || !(NOT) logical operators

--- LogicalOperators/src/Main.java
+++ LogicalOperators/src/Main.java
@@ -2,8 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//operatori logici sono utilizzati per connette 2 o piu operazioni
+		// && AND entrambi le condizioni devono essere vere
+		// || OR una delle condizione deve essere vera
+		// ! NOT inverte il valore di una condizione booleana (se era true diventa false e viceversa)
 	}
 
 }
